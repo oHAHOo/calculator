39549e3f4faf18a2edc02d1b3e7799af283e9c30
hjh <hjh690@naver.com>
2026-03-19 10:26:17 +0900

    Add : 기본 사칙연산 (정수, 연산자 입력)

--- src/main/java/org/zerock/Main.java
+++ src/main/java/org/zerock/Main.java
@@ -1,17 +1,42 @@
 package org.zerock;
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("첫번째 정수를 입력하세요.");
+        int firstNum = scanner.nextInt();
+        System.out.print("두번째 정수를 입력하세요.");
+        int secondNum = scanner.nextInt();
+
+        System.out.println("연산 기호를 입력하세요(+, -, *, /)");
+        char c = scanner.next().charAt(0);
+
+        switch (c)
+            {
+            case '+': {
+                System.out.println(firstNum + secondNum);
+                break;}
+            case '-': {
+                System.out.println(firstNum - secondNum);
+                break;
+            }
+            case '*': {
+                System.out.println(firstNum * secondNum);
+                break;
+            }
+            case '/': {
+                if(secondNum == 0 ){
+                    System.out.println("0으로 나눌 수 없습니다.");
+                    break;
+                }
+                System.out.println(firstNum / secondNum);
+                break;
+            }
+            }
+
         }
     }
-}
