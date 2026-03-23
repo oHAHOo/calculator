8630bc9097240c50a3e147c00cc662eff2d2ef30
hjh <hjh690@naver.com>
2026-03-23 09:51:25 +0900

    UPDATE : 주석추가

--- src/main/java/org/zerock/AppCalculator.java
+++ src/main/java/org/zerock/AppCalculator.java
@@ -2,17 +2,24 @@ package org.zerock;
 
 import java.util.ArrayList;
 
+
+//App.java용 계산결과 저장 클래스
 public class AppCalculator {
+
+    //결과값을 저장할 리스트 생성
     private ArrayList<String> list  = new ArrayList<>();
 
+    //리스트에 결과 추가
     public void setList(String result) {
         list.add(result);
     }
 
+    //리스트 반환
     public ArrayList<String> getList() {
         return list;
     }
 
+    //가장 최근 결과값 삭제
     public void deleteList(){
         list.remove(list.size()-1);
     }
--- src/main/java/org/zerock/OperatorType.java
+++ src/main/java/org/zerock/OperatorType.java
@@ -3,17 +3,20 @@ package org.zerock;
 import java.util.function.BiFunction;
 
 public enum OperatorType {
-    ADD((a, b) -> a + b),
-    SUBTRACT((a, b) -> a - b),
-    MULTIPLY((a, b) -> a * b),
-    DIVIDE((a, b) -> a / b);
+    //enum으로 사칙연산 정의
+    ADD((a, b) -> a + b), //덧셈
+    SUBTRACT((a, b) -> a - b), //뺄셈
+    MULTIPLY((a, b) -> a * b), //곱셈
+    DIVIDE((a, b) -> a / b); //나눗셈
 
+    //double로 숫자 2개를 받아서 double로 반환
     private BiFunction<Double, Double, Double> func;
 
     OperatorType(BiFunction<Double, Double, Double> func) {
         this.func = func;
     }
 
+    //계산 실행
     public double apply(double a, double b) {
         return func.apply(a, b);
     }
