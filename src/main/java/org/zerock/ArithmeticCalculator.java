faa8d735d117068fde97e064e1b60355842ea5c0
hjh <hjh690@naver.com>
2026-03-24 16:59:37 +0900

    UPDATE : 가장 먼저 저장된 값 삭제

--- src/main/java/org/zerock/ArithmeticCalculator.java
+++ src/main/java/org/zerock/ArithmeticCalculator.java
@@ -39,7 +39,7 @@ public class ArithmeticCalculator {
             System.out.println("메뉴를 선택하세요.\n");
             System.out.println("1. 계산 하기");
             System.out.println("2. 저장된 계산 결과 출력");
-            System.out.println("3. 최근 계산 결과 삭제");
+            System.out.println("3. 가장 먼저 저장된 값 삭제");
             System.out.println("4. 입력받은 값보다 큰 결과값 출력");
             System.out.println("5. 프로그램 종료");
 
